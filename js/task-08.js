dd372b6479820356e8ab0b24ecea4d022dba4c6d
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-02-26 20:24:58 +0200

    Сompleted tasks 8-9.

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const loginForm = document.querySelector("form.login-form");
 
+  loginForm.addEventListener('submit', event => {
+    event.preventDefault(); 
+
+    const formData = new FormData(loginForm);
+
+    const formValues = Object.fromEntries(formData.entries());
+
+    if (formValues.email.trim() === '' || formValues.password.trim() === '') {
+      alert("Please fill in all the fields!");
+    } else {
+      console.log(formValues); 
+      loginForm.reset();
+    }
+  });
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonEl = document.querySelector('.change-color');
+
+const colorSpanEl = document.querySelector('.color');
+
+buttonEl.addEventListener('click', function () {
+  
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  colorSpan.textContent = newColor;
+
+});
